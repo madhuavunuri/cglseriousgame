b672f372c27ccee35049c73b3b99f331938d445c
Madhu Avunuri <84817049+madhuopensesame@users.noreply.github.com>
2021-07-31 14:53:47 +0200

    All UI updated

--- Assets/Scripts/DisplayProfile.cs
+++ Assets/Scripts/DisplayProfile.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DisplayProfile : MonoBehaviour
 {
-    public Text info;
+
+    public Text nametxt;
+    public Text emailtxt;
+    public Text gendertxt;
+    public Text phonetxt;
+    public Text educationtxt;
+    public Text coursetxt;
+    public Text yearPassingtxt;
+    public Text studentIdtxt;
+    public Text skillstxt;
+
+
     // Start is called before the first frame update
     void OnEnable()
     {
-        info.text = "Name : " + CreateProfile.Instance.userProfile.name + "\n" +
-                    "Email : " + CreateProfile.Instance.userProfile.email + "\n" +
-                    "Phone : " + CreateProfile.Instance.userProfile.phone + "\n" +
-                    "Gender : " + CreateProfile.Instance.userProfile.gender + "\n" +
-                    "Education : " + CreateProfile.Instance.userProfile.study + "\n" +
-                    "Course : " + CreateProfile.Instance.userProfile.course + "\n" +
-                    "Year of Passing : " + CreateProfile.Instance.userProfile.batchYear + "\n" +
-                    "Student ID : " + CreateProfile.Instance.userProfile.studentID + "\n" +
-                    "Skills : " + CreateProfile.Instance.userProfile.skillset + "\n";
+        nametxt.text = "Name : " + CreateProfile.Instance.userProfile.name;
+        emailtxt.text = "Email : " + CreateProfile.Instance.userProfile.email;
+        gendertxt.text = "Gender : " + CreateProfile.Instance.userProfile.gender;
+        phonetxt.text = "Phone : " + CreateProfile.Instance.userProfile.phone;
+        educationtxt.text = "Education : " + CreateProfile.Instance.userProfile.study;
+        coursetxt.text = "Course : " + CreateProfile.Instance.userProfile.course;
+        yearPassingtxt.text = "Year of Passing : " + CreateProfile.Instance.userProfile.batchYear;
+        studentIdtxt.text = "Student ID : " + CreateProfile.Instance.userProfile.studentID;
+        skillstxt.text = "Skills : " + CreateProfile.Instance.userProfile.skillset;
     }
 
 }
